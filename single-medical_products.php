5446665c1ed8da5771e16556be247d50c4c6bd4a
CodeRiderz <chiputera@gmail.com>
2018-07-04 22:32:50 +0700

    remove bottom meta wrapper

--- single-medical_products.php
+++ single-medical_products.php
@@ -127,26 +127,6 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 						wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
 					?>
 					</div> <!-- .entry-content -->
-					<div class="et_post_meta_wrapper">
-					<?php
-					if ( et_get_option('divi_468_enable') == 'on' ){
-						echo '<div class="et-single-post-ad">';
-						if ( et_get_option('divi_468_adsense') <> '' ) echo( et_get_option('divi_468_adsense') );
-						else { ?>
-							<a href="<?php echo esc_url(et_get_option('divi_468_url')); ?>"><img src="<?php echo esc_attr(et_get_option('divi_468_image')); ?>" alt="468" class="foursixeight" /></a>
-				<?php 	}
-						echo '</div> <!-- .et-single-post-ad -->';
-					}
-				?>
-
-					<?php if (et_get_option('divi_integration_single_bottom') <> '' && et_get_option('divi_integrate_singlebottom_enable') == 'on') echo(et_get_option('divi_integration_single_bottom')); ?>
-
-					<?php
-						if ( ( comments_open() || get_comments_number() ) && 'on' == et_get_option( 'divi_show_postcomments', 'on' ) ) {
-							comments_template( '', true );
-						}
-					?>
-					</div> <!-- .et_post_meta_wrapper -->
 				</article> <!-- .et_pb_post -->
 
 			<?php endwhile; ?>
