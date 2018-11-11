83939d30627c7bf775abd93353535dce1a43e6f8
CodeRiderz <chiputera@gmail.com>
2018-11-11 13:52:33 +0700

    test chart js

--- js/chifront.js
+++ js/chifront.js
@@ -24,6 +24,21 @@
         showTargetInCenterServiceBox(targetId)
       })
     }
+
+    const serviceChartCtx = document.getElementById('ServicesChart');
+    const serviceChart = new Chart(ServiceUIFrameContext, {
+      type: 'doughnut',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets:[
+          {
+            label: "My First Dataset",
+            data: [300,50,100],
+            backgroundColor: ["rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)"]
+          }
+        ]
+      }
+    });
   })
 
   function showTargetInCenterServiceBox (targetId) {
